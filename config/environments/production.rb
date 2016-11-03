4046ea08394bdfe524b06256081c9215d4bc6067
Michael Furr <mfurr14@uncc.edu>
2016-11-03 22:20:00 +0000

    add user functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { host: ' https://powerful-brook-42361.herokuapp.com/' }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
