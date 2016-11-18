20ced5fb6e0f20cfa83c234b2e97768bda3d0f8f
Michael Furr <mfurr14@uncc.edu>
2016-11-18 02:00:12 +0000

    regen of simple_form to fix error

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,7 +1,9 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   
+  #added per note 2
   config.input_class = "form-control"
+  
   
   # Wrappers are used by the form builder to generate a
   # complete input. You can remove any component from the
